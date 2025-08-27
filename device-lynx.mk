2040cb38889973edb3bc6e83a649c09b500c277c
Tam Huynh <115771465+doanvtamhuynh@users.noreply.github.com>
2025-08-27 10:52:43 +0700

    Update kernel dir 25Q1-13202328

--- device-lynx.mk
+++ device-lynx.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 #
 
-TARGET_KERNEL_DIR := device/google/lynx-kernels/6.1/25Q1-12919773
-TARGET_BOARD_KERNEL_HEADERS := device/google/lynx-kernels/6.1/25Q1-12919773/kernel-headers
-TARGET_PREBUILT_KERNEL := device/google/lynx-kernels/6.1/25Q1-12919773/Image.lz4
+TARGET_KERNEL_DIR := device/google/lynx-kernels/6.1/25Q1-13202328
+TARGET_BOARD_KERNEL_HEADERS := device/google/lynx-kernels/6.1/25Q1-13202328/kernel-headers
+TARGET_PREBUILT_KERNEL := device/google/lynx-kernels/6.1/25Q1-13202328/Image.lz4
 
 DEVICE_PACKAGE_OVERLAYS += device/google/lynx/lynx/overlay
 
